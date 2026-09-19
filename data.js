7b90c5b36897a4a63d808b13a03c5c0f8fa4fc70
palmasscratch <chengpalmas@gmail.com>
2026-09-19 19:24:42 +0800

    Update data.js

--- data.js
+++ data.js
@@ -4,20 +4,20 @@ const studentProjects = [
         studentName: "ming",
         projects: [
             // path 可以是相對路徑，或是 Scratch 官方的網址
-            { title: "尋找飛貓寶寶", path: "./projects/ming/ch6ex2.html" }
+            { title: "雞蛋蛋音符ex2", path: "./projects/ming/ch6ex2.html" }
         ]
     },
     {
         studentName: "palmas",
         projects: [
-            { title: "尋找飛貓寶寶", path: "./projects/palmas/ch6ex2.html" }
+            { title: "雞蛋蛋音符ex2", path: "./projects/palmas/ch6ex2.html" }
         ]
     },
     {
         studentName: "siaoyin",
         projects: [
             // 如果作品是放在 Scratch 網站上，也可以直接貼網址
-            { title: "尋找飛貓寶寶", path: "./projects/siaoyin/ch6ex2.html" }
+            { title: "雞蛋蛋音符ex2", path: "./projects/siaoyin/ch6ex2.html" }
         ]
     }
     // 若要新增學生，請在此處繼續添加
