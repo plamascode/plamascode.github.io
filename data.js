74d6b31d99770b60712a8d6c21dbae3886e32004
palmasscratch <chengpalmas@gmail.com>
2026-09-20 17:07:11 +0800

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,9 @@ const studentProjects = [
         projects: [
             // path 可以是相對路徑，或是 Scratch 官方的網址
             { title: "雞蛋蛋音符ex2", path: "./projects/ming/ch6ex2.html" },
-            { title: "量角器", path: "./projects/ming/angle.html" }
+            { title: "量角器", path: "./projects/ming/angle.html" },
+             { title: "PokemonBattle", path: " https://drive.google.com/file/d/1CSdL-0IGBpGBNnZflL-QJc1VMSJZ335l/view?usp=sharing" }
+
         ]
     },
     {
